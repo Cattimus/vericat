076c8584c2d165244c53a805c0a1949fbb4199ec
Cattimus <github_commit@cattimus.dev>
2024-04-02 14:52:35 -0700

    Added singleton class for state management

--- vericat.py
+++ vericat.py
@@ -1,17 +1,60 @@
 import hashlib
 
-filename = "vericat.py"
-f = open(filename, "rb")
-data = f.read()
+class vericat:
+	#for -i(nput file) option
+	input_path = ""
+	input_filename = ""
 
-md5 = hashlib.md5(data)
-sha1 = hashlib.sha1(data)
-sha256 = hashlib.sha256(data)
-sha384 = hashlib.sha384(data)
-sha512 = hashlib.sha512(data)
+	#for --algo=md5,sha256,sha... option
+	algo_list = ["md5", "sha1", "sha256", "sha384", "sha512"]
 
-print("md5: ",    md5.hexdigest())
-print("sha1: ",   sha1.hexdigest())
-print("sha256: ", sha256.hexdigest())
-print("sha384: ", sha384.hexdigest())
-print("sha512: ", sha512.hexdigest())
+	#for -o(utput) option
+	output_path = ""
+	output_filename = ""
+
+	#for -f (file format) option
+	file_format = False
+
+	def gen_hashes(self):
+		output = ""
+
+		#get file data
+		f = open(self.input_path, "rb")
+		data = f.read()
+		f.close()
+
+		#iterate through the selected algorithms
+		for algo in self.algo_list:
+			hash = None
+
+			match algo:
+				case "md5":
+					hash = hashlib.md5
+				case "sha1":
+					hash = hashlib.sha1
+				case "sha256":
+					hash = hashlib.sha256
+				case "sha384":
+					hash = hashlib.sha384
+				case "sha512":
+					hash = hashlib.sha512
+		
+			#list an error if the algorithm is unsupported
+			if hash == None:
+				output += algo + ": " + "Unsupported"
+				continue
+
+			#list output with proper formatting
+			if self.file_format:
+				output += hash(data).hexdigest() + " " + self.input_filename + "\n"
+			else:
+				output += algo + ": " + hash(data).hexdigest() + "\n"
+		
+		return output
+
+cat = vericat()
+cat.input_path = "vericat.py"
+cat.input_filename = "vericat.py"
+
+output = cat.gen_hashes()
+print(output)
