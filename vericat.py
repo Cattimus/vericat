0dea185043a80c3b2d1f9722b8cdfad9d705c8b9
Cattimus <github_commit@cattimus.dev>
2024-04-04 00:24:52 -0700

    Updated text; Added ability to truncate path in hash generation

--- vericat.py
+++ vericat.py
@@ -13,22 +13,12 @@ hashes = {
 	128: "sha512"
 }
 
-#map algorithm names to hashing functions
-algorithms = {
-	"md5": hashlib.md5,
-	"sha1": hashlib.sha1,
-	"sha256": hashlib.sha256,
-	"sha384": hashlib.sha384,
-	"sha512": hashlib.sha512
-}
-
 class vericat:
 	#pattern match for hashes in file
 	pattern = r"([0-9a-fA-F]+) +(\S+)"
 
 	#for -i(nput file) option
 	input_path = ""
-	input_filename = ""
 
 	#for --algo=md5,sha256,sha... option
 	algo_list = ["md5", "sha1", "sha256", "sha384", "sha512"]
@@ -41,46 +31,47 @@ class vericat:
 
 	#for -f (file format) option
 	file_format = False
+	
+	#for -t (truncate path) option
+	truncate_path = False
 
 	#attempt to identify hashing algorithm by length of hash
 	def identify_hash(self, hash):
 		l = len(hash)
 		if l in hashes:
-			print("Detected hashing algorithm: ", hashes[l], "[", hash, "]")
+			print("Detected algorithm from hash: " + hashes[l] + " [" + hash + "]")
 			return hashes[l]
 		
-		print("Unable to detect hashing algorithm based on input", file=sys.stderr)
+		print("Unable to detect hashing algorithm based on input: [", hash, "]", file=sys.stderr)
 		return None
 
 	#check hash for a single algorithm
-	def check_hash(self, data, hash):
+	def check_hash(self, path, hash):
 		algo = self.identify_hash(hash)
 		if algo == None:
 			return False
 
-		reference_hash = self.gen_hash(data, algo)
+		#reference hash is a known good hash that has been computed by our program
+		reference_hash = self.gen_hash(path, algo)
 		
 		if reference_hash == hash:
-			print("Match.")
+			print("Hashes match.")
 		else:
-			print("MISMATCH: ", reference_hash)
+			print("HASH MISMATCH: ", reference_hash)
 
 	#check all hashes from a file
 	#by default, it will get the name of the file from the hash file on disk
 	#can also optionally accept a filepath if the hash file is in another directory
 	def check_hashes(self, hash_path, file_path=None):
-		file_data = None
 		hash_data = None
+
+		if file_path != None:
+			print("Checking hashes for file: " + file_path + "...\n")
 
 		#standardize path to *nix
 		hash_path = hash_path.replace("\\", "/")
 
-		#get file binary data from provided path
-		if file_path != None:
-			f = open(file_path, "rb")
-			file_data = f.read()
-			f.close()
-
+		#TODO - Handle files that don't exist gracefully
 		#get list of hashes from hashfile
 		f = open(hash_path, "r")
 		hash_data = f.read()
@@ -105,36 +96,46 @@ class vericat:
 					base_path = hash_path[:end_index]
 				file_path = base_path + info[1]
 
-				#read data from file
-				f = open(file_path, "rb")
-				file_data = f.read()
-				f.close()
+				print("Checking hashes for file: " + file_path + "...\n")
 			
 			#check hash
 			hash = info[0]
-			self.check_hash(file_data, hash)
-		
+			self.check_hash(file_path, hash)
 		return
 
-	#gen hash for single algorithm
-	def gen_hash(self, data, algo):
-		if not algo in algorithms:
+	#TODO - Handle files that don't exist gracefully
+	#hash file from path
+	def gen_hash(self, path, algo):
+		if not algo in hashlib.algorithms_available:
+			print("Hashing algorithm is not supported.")
 			return None
 		
-		return algorithms[algo](data).hexdigest()
+		handle = open(path, "rb")
+		hash = hashlib.file_digest(handle, algo).hexdigest()
+		handle.close()
+		return hash
 				
-	def gen_hashes(self, data):
+	def gen_hashes(self, path):
 		self.output_data = ""
+		print("Generating hashes for file: " + path + "...\n")
 
 		#iterate through the selected algorithms
 		for algo in self.algo_list:
 
 			#generate hash for the algorithm
-			hash = self.gen_hash(data, algo)
+			hash = self.gen_hash(path, algo)
+
+			#truncate the inner path of file
+			final_path = path
+			if self.truncate_path:
+				i = final_path.rfind("/")
+				if i == -1:
+					i = final_path.rfind("\\")
+				final_path = final_path[i:]
 
 			#list output with proper formatting
 			if self.file_format:
-				self.output_data += hash + " " + self.input_filename + "\n"
+				self.output_data += hash + " " + path + "\n"
 			else:
 				self.output_data += algo + ": " + hash + "\n"
 
@@ -151,6 +152,7 @@ class vericat:
 
 cat = vericat()
 
+'''
 cat.input_path = "test.cat"
 cat.input_filename = "test.cat"
 
@@ -160,8 +162,10 @@ cat.output_filename = "test2.cat"
 
 #read data from file
 f = open(cat.input_path, "rb")
-data = f.read()
-f.close()
 
-cat.gen_hashes(data)
+cat.gen_hashes(f)
+f.close()
 cat.write_output()
+'''
+
+cat.check_hashes("test.cat")
